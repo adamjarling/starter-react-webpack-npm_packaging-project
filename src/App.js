786ac10c977db21e80f1d9bb8e87c74c6d1e22db
Adam Arling <adam.arling@northwestern.edu>
2020-01-14 13:05:44 -0600

    Remove class component and update Prettier settings

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 // Font Awesome Imports
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faPen, faSave, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPen, faSave, faTrash } from "@fortawesome/free-solid-svg-icons";
 library.add(faPen, faSave, faTrash);
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Your app goes here</h1>
-        <p>
-          CSS styles are applied like <span className="some-style">this</span>
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <h1>Your app goes here</h1>
+      <p>
+        CSS styles are applied like <span className="some-style">this</span>
+      </p>
+    </div>
+  );
+};
 
 export default App;
